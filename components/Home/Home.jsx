87ce65b54795fc7ffc976358e4e540756ea47ded
Rick Henry <fredericmhenry@gmail.com>
2019-04-20 12:52:39 -0400

    fixing a typo
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -60,7 +60,7 @@ class Home extends Component {
     return (
       <div className="lander">
         <h1>Scratch</h1>
-        <p>A very expensive note taking app</p>
+        <p>A simple note taking app</p>
         <Link to="/login" className="btn btn-info btn-lg lander-button">
           Login
         </Link>
